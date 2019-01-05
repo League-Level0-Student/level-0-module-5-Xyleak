07bc52a39ba9a186dc1a1780c6618263a8f6bb44
CesarTadeo <league@wts22.attlocal.net>
2019-01-05 12:54:28 -0800

    For Loop

--- src/nested_loops/For_Loop_Gauntlet.java
+++ src/nested_loops/For_Loop_Gauntlet.java
@@ -1,190 +1,88 @@
 package nested_loops;
 
 public class For_Loop_Gauntlet {
-	
-public static void main(String[] args) {
-	
-	for (int i = 0; i < 101; i++) {
-		System.out.println(i);
-	
-		
-}
-	
-	for (int e = 100; e >= 0; e--) {
-		System.out.println(e);
-		
-}
-	
-	for (int o = 2; o < 101; o++) { 
-	System.out.println(o);
-	}
-	
-	for (int u = 1; u < 100;  u++) {
-		System.out.println(u);
-		
-	}
-		for(int x = 1; x < 501; x++) {
-			System.out.println(x);
+
+	public static void main(String[] args) {
+		System.out.println("1---------------------------------");
+		for (int i = 0; i < 101; i++) {
+			System.out.println(i);
+
 		}
-	for(int a = 0; a  < 777; a++  ) {
-	System.out.println(a);
-	}
-	
-	
-//Nested For-Loops_________________________________________________________
-	
-	
+		System.out.println("2---------------------------------");
+		for (int e = 100; e >= 0; e--) {
+			System.out.println(e);
 
-// Nested For-Loops 1		
-
-	//1
-	for (int b = 0; b < 1; b++  ) {
-
-		for (int c = 0; c < 1; c++  ) {
-		
-		System.out.print(b);
-		System.out.println(c);
 		}
-		}
-		// 2
-		for (int c = 0; c < 1; c++  ) {
-		
+		System.out.println("3---------------------------------");
+		for (int o = 2; o < 101; o += 2) {
 
-			for (int by = 1; by < 2; by++  ) {
-				
-			System.out.print(c);
-			System.out.println(by);
+			System.out.println(o);
+		}
+
+		System.out.println("4---------------------------------");
+		for (int u = 1; u < 100; u += 2) {
+			System.out.println(u);
+
+		}
+		System.out.println("5---------------------------------");
+		for (int x = 1; x < 501; x++) {
+			// System.out.println(x);
+
+			if (x % 2 == 0) {
+				System.out.println(x + " Is Even");
+			} else {
+				System.out.println(x + " Is Odd");
 			}
 		}
-			//3
-			
-			for (int cv = 0; cv < 1; cv++  ) {
-				
+		System.out.println("6---------------------------------");
+		for (int a = 0; a < 777; a += 7) {
+			System.out.println(a);
+		}
 
-				for (int bye = 2; bye < 3; bye++  ) {
-					
-				System.out.print(cv);
-				System.out.println(bye);
-				}
+		// Nested For-Loops_________________________________________________________
+
+		// Nested For-Loops 1
+
+		// 1
+		System.out.println("1 Nested----------");
+		for (int b = 0; b <= 2; b++) {
+
+			for (int c = 0; c <= 2; c++) {
+
+				System.out.println(b + " " + c);
+
 			}
-			
-//4
-			
-			for (int cv = 1; cv < 2; cv++  ) {
-				
+		}
 
-				for (int bye = 0; bye < 1; bye++  ) {
-					
-				System.out.print(cv);
-				System.out.println(bye);
-				}
-			}
-			
-//5
-			
-			for (int cv = 1; cv < 2; cv++  ) {
-				
+		// Nested For-Loops 2
 
-				for (int bye = 1; bye < 2; bye++  ) {
-					
-				System.out.print(cv);
-				System.out.println(bye);
-				}
-			}
-			
-//6
-			
-			for (int cv = 1; cv < 2; cv++  ) {
-				
+		// 1
+		System.out.println("2 Nested-----------");
+		for (int i = 1; i < 4; i++) {
+			System.out.print(i + " ");
 
-				for (int bye = 2; bye < 3; bye++  ) {
-					
-				System.out.print(cv);
-				System.out.println(bye);
-				}
-			}
-			
-//7
-			
-			for (int cv = 2; cv < 3; cv++  ) {
-				
+			if (i % 3 == 0) {
+				System.out.println();
+				for (int j = 1; j <= 9; j += 3) {
 
-				for (int bye = 0; bye < 1; bye++  ) {
-					
-				System.out.print(cv);
-				System.out.println(bye);
-				}
-			}
-			
-//8
-			
-			for (int cv = 2; cv < 3; cv++  ) {
-				
+					System.out.print(j + " ");
 
-				for (int bye = 1; bye < 2; bye++  ) {
-					
-				System.out.print(cv);
-				System.out.println(bye);
-				}
-			}
-			
-//9
-			
-			for (int cv = 2; cv < 3; cv++  ) {
-				
+					for (int j2 = 1; j2 <= j + 10; j2++) {
+						System.out.println(j + " ");
 
-				for (int bye = 2; bye < 3; bye++  ) {
-					
-				System.out.print(cv);
-				System.out.println(bye);
-				}
-			
-//Nested For-Loops 2
-				
-				
-				//1
-				
-				for (int ab = 1; ab < 4; ab++  ) {
-			
-					System.out.print(ab);
-					
 					}
+					System.out.println();
 				}
-			
-			
-			System.out.println();
-			
-			
-			//2
-			
-			for (int abc = 4; abc < 7; abc++  ) {
-		
-				System.out.print(abc);
-				
-				}
-		
 
-System.out.println();
+			}
+		}
 
+		// Nested For-Loops 3
 
-//3
+		for (int i = 1; i < 11; i++) {
+			System.out.print(i + " ");
 
-for (int abcd = 7; abcd < 10; abcd++  ) {
+		}
 
-	System.out.print(abcd);
-	
 	}
 }
-	
-Nested For-Loops 3
-				
-				
-				
-				
-			}
-			
-			
-			
-
-	
-	
