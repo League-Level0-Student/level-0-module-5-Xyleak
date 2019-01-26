e6a61ebcae538300ce8225be3b4a7508989b3c43
league <league@wts14-2.attlocal.net>
2019-01-26 12:58:32 -0800

    Skill

--- src/algorithms/SkillPractice.java
+++ src/algorithms/SkillPractice.java
@@ -3,30 +3,24 @@ package algorithms;
 import javax.swing.JOptionPane;
 
 public class SkillPractice {
+	
 public static void main(String[] args){
-
-SkillPractice skills = new SkillPractice();	
-
-skills.skill1();	
-skills.skill2();	
-//skills.skill3();	
-//skills.skill4();	
-//skills.skill5();	
-
+	SkillPractice skills = new SkillPractice();	
+	skills.skill1();
 }
 
-void skill1(){
-String Dimes = JOptionPane.showInputDialog("How Many Dimes Do You Have?");
-int Cents = Dimes*10
-JOptionPane.showMessageDialog(null, Cents+" Cents");
+void skill1() {
+	String D = JOptionPane.showInputDialog("How Many Dimes Do You Have?");
+	JOptionPane.showMessageDialog(null, "You have "+ 10*Integer.parseInt(D)+" cents.");
 
+	String C = JOptionPane.showInputDialog("How Tall Are You In Inches?");
+	if (Integer.parseInt(C) < 36) { JOptionPane.showMessageDialog(null, " To Eat Their Wheaties");
+}
+
+void skill2() {
 	
+	for (int i = 0; i < array.length; i++) {
+		
+	}	
 }
-
-
-void skill2(){
-
-	
-}
-
 }
